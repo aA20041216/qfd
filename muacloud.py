0d2a14165bc53b4620f596e1f9b0a55d85418ef8
aA20041216 <80548464+aA20041216@users.noreply.github.com>
2025-11-25 23:00:38 +0800

    Update muacloud.py

--- muacloud.py
+++ muacloud.py
@@ -1,6 +1,6 @@
 import requests
 try:
-    response = requests.get("https://www.skymz.com/#/login", timeout=10)
+    response = requests.get("https://baidu.com", timeout=10)
     print(f"连接状态: {response.status_code}")
 except Exception as e:
     print(f"连接失败: {e}")
